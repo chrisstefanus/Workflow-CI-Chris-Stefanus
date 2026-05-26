b6b668d506bd8aaaacc331cac31a586a5eab57cf
Christianto Stefanus <64879901+chrisstefanus@users.noreply.github.com>
2026-05-26 14:00:10 +0700

    Create modelling.py

---
+++ MLProject/modelling.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("titanic_preprocessed.csv")
+
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+mlflow.autolog()
+
+with mlflow.start_run():
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=5,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(
+        y_test,
+        y_pred
+    )
+
+print("Accuracy:", accuracy)
